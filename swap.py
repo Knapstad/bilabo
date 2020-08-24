59671d86b72dee3480f13b2d55e108cb980769de
Bendik <knaben@OBOS.NO>
2020-08-24 15:03:23 +0200

    added data according to schema

--- swap.py
+++ swap.py
@@ -3,11 +3,12 @@ from bs4 import BeautifulSoup as BS
 
 
 class Swap:
-    def __init__(self, postalcode: str):
+    def __init__(self, postalcode: str = None):
         pass
 
     @classmethod
     def get_cars(cls, postalcode=None):
+        base = "https://swapcar.no"
         api = "https://swapacar.no/start"
         response = requests.get(f"{api}")
         tries = 0
@@ -20,12 +21,22 @@ class Swap:
         available = []
         cars = soup.findAll("div", {"class": "fcar-list"})
         for car in cars:
+            details = car.find("div", {"class": "fcar-sdesc"}).text.split("|")
             available.append(
                 {
-                    "title": car.find("div", {"class": "fcar-title"}).text,
+                    "name": car.find("div", {"class": "fcar-title"}).text,
+                    "make": car.find("div", {"class": "fcar-title"}).text.split()[0],
+                    "model": car.find("div", {"class": "fcar-title"}).text.split()[1:],
+                    "drive": details[2],
+                    "year": details[0],
+                    "seats": details[1],
+                    "transmission": details[3],
                     "price": car.__dict__["attrs"]["data-price"],
-                    "transmission": car.__dict__["attrs"]["data-transmission"],
-                    "fuel": car.__dict__["attrs"]["data-powertrain"],
+                    "distance": details[-1],
+                    "location": "Oslo",
+                    "availability": "Available",
+                    "order": f'{base}{car.find("div", {"class": "fcar-btn"}).a["href"]}',
+                    "img": car.find("img")["src"],
                 }
             )
         return (available, None)
