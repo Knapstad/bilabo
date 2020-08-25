476c5891d67fb295c9bf326e03ab2bbbbb1415c8
Bendik <knaben@OBOS.NO>
2020-08-25 14:46:51 +0200

    add site

---
+++ site/src/components/Car.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <div class="column">
+      <p>{{ car.name }}</p>
+      <p>{{ car.site }}</p>
+      <p>{{ car.price }}</p>
+    </div>
+    <div class="column">
+      <img :src="car.img.replace(' ','-')" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    car: Object,
+    site: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  width: 100%;
+  margin: auto;
+}
+.home {
+  width: 100%;
+}
+.column {
+  float: right;
+  width: 50%;
+  margin: auto;
+}
+img {
+  width: 100%;
+}
+</style>
---
+++ site/src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <div class="carcontainer">
+      <div v-for="(car, index) in Object.values(cars).flat()" :key="index">
+        <Car :car="car" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Car from "@/components/Car.vue";
+import cars from "../../../mycars.json";
+
+export default {
+  name: "Home",
+  components: {
+    Car
+  },
+  data() {
+    return {
+      cars
+    };
+  },
+  computed: {
+    flatcars: function() {
+      var flatcars = [];
+      for (const value in Object.entries(cars)) {
+        flatcars.push(value);
+      }
+      return flatcars;
+    }
+  }
+};
+</script>
+<style>
+.carcontainer {
+  display: inline-grid;
+  grid-template-columns: 30% 30% 30%;
+  padding: 30px;
+  margin: 10px;
+}
+.car {
+  /* background-color: rgba(255, 255, 255, 0.8); */
+  border: 1px solid;
+  /* padding: 20px; */
+  /* font-size: 30px; */
+  /* text-align: center; */
+}
+</style>
