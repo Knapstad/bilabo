a67650b5874064686351539d0399efdac66910f2
Bendik <knaben@OBOS.NO>
2020-08-27 12:42:07 +0200

    added try except, returnes none when failes

--- cloudFuctions/imove.py
+++ cloudFuctions/imove.py
@@ -10,47 +10,56 @@ class Imove:
 
     @classmethod
     def get_cars(cls, postalcode=None):
-        headers = {"User-Agent": "My User Agent 1.0"}
-        base = "https://secure.imove.no/cars"
-        api = "https://secure.imove.no/api/vehicles"
-        response = requests.get(f"{api}", headers=headers)
-        tries = 0
-        while "20" not in str(response.status_code):
+        try:
+            base = "https://secure.imove.no/cars"
+            api = "https://secure.imove.no/api/vehicles"
             response = requests.get(f"{api}")
-            tries += 1
-            if tries > 3:
-                break
-        if "20" not in str(response.status_code):
-            print("no response")
-            print(response)
-            print(response.text)
-        cars = response.json()
-        cleanCars = []
-        for car in cars:
-            cleanCars.append(
-                {
-                    "site": "imove",
-                    "name": f"{car['make']} {car['model']}",
-                    "make": car["make"],
-                    "model": car["model"],
-                    "drive": car["fuelType"],
-                    "year": car["year"],
-                    "seats": car["numberOfSeats"],
-                    "transmission": "",
-                    "price": int(car["pricePerMonth"]),
-                    "range": car["range"],
-                    "kmMonth": "",
-                    "location": [
-                        district["description"] for district in car["districts"]
-                    ],
-                    "availability": "available" if car["isReserved"] else "unavailable",
-                    "order": f'{base}/{car["id"]}',
-                    "img": f'{base}{car["images"][0]["url"]}' if car["images"] else "",
-                    "cargoVolume": car["trunkCapacityInLiters"],
-                }
-            )
+            tries = 0
+            while "20" not in str(response.status_code):
+                response = requests.get(f"{api}")
+                tries += 1
+                if tries > 3:
+                    print("no response imove")
+                    break
+            if "20" not in str(response.status_code):
+                return None
+            cars = response.json()
+            cleanCars = []
+            for car in cars:
+                cleanCars.append(
+                    {
+                        "site": "imove",
+                        "name": f"{car['make']} {car['model']}",
+                        "make": car["make"],
+                        "model": car["model"],
+                        "drive": "Elekrtisk"
+                        if car["fuelType"] == "el"
+                        else car["fuelType"],
+                        "year": car["year"],
+                        "seats": car["numberOfSeats"],
+                        "transmission": "",
+                        "price": int(car["pricePerMonth"]),
+                        "range": car["range"],
+                        "kmMonth": "",
+                        "location": [
+                            district["description"] for district in car["districts"]
+                        ],
+                        "availability": "available"
+                        if car["isReserved"]
+                        else "unavailable",
+                        "order": f'{base}/{car["id"]}',
+                        "img": f'{base}{car["images"][0]["url"]}'
+                        if car["images"]
+                        else "",
+                        "cargoVolume": car["trunkCapacityInLiters"],
+                    }
+                )
 
-        available = [car for car in cleanCars if car["availability"] == "available"]
-        unavailable = [car for car in cleanCars if car["availability"] != "available"]
-        return (available, unavailable)
-
+            available = [car for car in cleanCars if car["availability"] == "available"]
+            unavailable = [
+                car for car in cleanCars if car["availability"] != "available"
+            ]
+            return (available, unavailable)
+        except Exception as e:
+            print(e)
+            return None
