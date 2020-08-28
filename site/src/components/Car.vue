6def8231362552734dd126a4cf23833f1508c158
Bendik <knaben@OBOS.NO>
2020-08-28 10:33:47 +0200

    added url change

--- site/src/components/Car.vue
+++ site/src/components/Car.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="column">
         <svg
-          v-if="car.site === 'imove'"
+          v-if="car.img === 'svg'"
           xmlns="http://www.w3.org/2000/svg"
           xmlns:xlink="http://www.w3.org/1999/xlink"
           version="1.1"
