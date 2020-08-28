e037cdcc5903c44bdfca1dcb8f789ccb7723bfc9
Bendik <knaben@OBOS.NO>
2020-08-28 18:39:16 +0200

    created filters

---
+++ site/src/components/Filters.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="filter-wrap">
+    Sted:
+    <div v-for="item in items" :key="item">
+      <FilterItem :item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import FilterItem from "@/components/FilterItem.vue";
+
+export default {
+  name: "Filters",
+  components: {
+    FilterItem
+  },
+  data() {
+    return {
+      items: ["Oslo", "Bergen", "Stavanger"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.filter-wrap {
+  display: flex;
+}
+</style>
