36b39b874db20923c4f5d73b699733a745222fab
Bendik <knaben@OBOS.NO>
2020-08-28 18:39:05 +0200

    Created filter item

---
+++ site/src/components/FilterItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="filter" :class="{active: isActive}" @click="active">{{item}}</div>
+</template>
+
+<script>
+export default {
+  name: "FilterItem",
+  props: ["item"],
+  data() {
+    return {
+      isActive: false
+    };
+  },
+  methods: {
+    active: function() {
+      if (this.isActive) {
+        this.$store.commit("removeLocation", this.item);
+      }
+      if (!this.isActive) {
+        this.$store.commit("addLocation", this.item);
+      }
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.filter-wrap {
+  display: flex;
+}
+.filter {
+  border-radius: 2px;
+  background: var(--main-medium-dark);
+  color: white;
+  padding: 5px;
+  margin: 5px;
+}
+.active {
+  background: var(--main-medium-light);
+  color: black;
+  padding: 5px;
+  margin: 5px;
+}
+</style>
