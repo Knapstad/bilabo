268538c189f01650fdce044434c7d3b365c46bd3
Bendik <knaben@OBOS.NO>
2020-09-01 13:58:16 +0200

    added footer component

---
+++ site/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="contact">
+    Contact: <a href="mailto:post@bilabonnement.app">post@bilabonnement.app</a>
+  </div>
+</template>
+
+<script>
+// import FilterItem from "@/components/FilterItem.vue";
+
+export default {
+  name: "Foot"
+};
+</script>
+
+<style scoped>
+.contact {
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+  background: var(--main-dark);
+  color: white;
+  font-size: 30px;
+}
+
+a {
+  color: white;
+}
+
+</style>
