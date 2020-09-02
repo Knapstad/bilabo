b58426159f8629be8c52da0ec6b920c1a08088b8
Bendik <knaben@OBOS.NO>
2020-09-02 12:56:00 +0200

    added dynamic locations

--- site/src/components/Filters.vue
+++ site/src/components/Filters.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="filter-wrap">
     <div class="label">Sted:</div>
-    <div v-for="item in items" :key="item">
+    <div v-for="item in locations" :key="item">
       <FilterItem :item="item" />
     </div>
   </div>
@@ -9,17 +9,27 @@
 
 <script>
 import FilterItem from "@/components/FilterItem.vue";
+// console.log(item);
 
 export default {
   name: "Filters",
   components: {
-    FilterItem
+    FilterItem,
   },
+  props: ["data"],
   data() {
     return {
-      items: ["Oslo", "Bergen", "Stavanger"]
+      items: ["Oslo", "Bergen", "Stavanger"],
     };
-  }
+  },
+  computed: {
+    locations: function () {
+      let cars = Object.values(this.data["data"]).flat();
+      cars = cars.map((a) => a.location).flat().filter((v, i, a) => a.indexOf(v) === i);
+
+      return cars;
+    },
+  },
 };
 </script>
 
