4c9ff17a1ce170a1fa3e56bb1ad11bc3e092f0da
Bendik <knaben@OBOS.NO>
2020-09-03 13:20:04 +0200

    added makes and uppdated filter onclick

--- site/src/components/Filters.vue
+++ site/src/components/Filters.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="filter-wrap">
     <div class="label">Sted:</div>
-    <div v-for="item in locations()" :key="item">
-      <FilterItem :item="item" />
+    <div class="filter-section">
+      <div v-for="item in locations()" :key="item">
+        <FilterItem :item="item" :type="'locations'" />
+      </div>
+    </div>
+    <div class="label">Merke:</div>
+    <div class="filter-section">
+      <div v-for="item in makes()" :key="item">
+        <FilterItem :item="item" :type="'makes'" />
+      </div>
     </div>
   </div>
 </template>
@@ -31,18 +39,36 @@ export default {
         .filter((v, i, a) => a.indexOf(v) === i);
       return cars;
     },
+    makes: function () {
+      let cars = Object.values(this.data["data"]).flat();
+      cars = cars
+        .map((a) => a.make)
+        .flat()
+        .filter((v, i, a) => a.indexOf(v) === i);
+      return cars;
+    },
   },
 };
 </script>
 
 <style scoped>
 .filter-wrap {
+  margin: auto;
   display: flex;
-  justify-content: center;
-  padding-top: 50px;
+  padding: 1rem;
+  width: fit-content;
+  background: var(--main-medium-light);
+  color: white;
+  margin-top: 1rem;
+  box-shadow: 2px 2px 3px #000;
+  border-radius: 2px;
+}
+.filter-section {
+  display: grid;
+  grid-template-rows: repeat(3, 2.2rem);
+  grid-auto-flow: column;
 }
 .label {
-  margin-top: auto;
-  margin-bottom: auto;
+  margin-left: 1rem;
 }
 </style>
