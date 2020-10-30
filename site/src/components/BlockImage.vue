a76f5047eeb0e08b8b1af20f469d70e1a6c3edd0
Bendik Knapstad <knaben@OBOS.NO>
2020-10-30 15:42:26 +0100

    added block image

---
+++ site/src/components/BlockImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <img :src="imgUrl" :alt=alt :class=clss>
+  </div>
+  
+
+</template>
+
+<script>
+export default {
+
+    props: [ "_key", "_type", "alt", "asset", "caption", "clss", "_sanityProps" ],
+    computed: {
+        imgUrl() {
+            if(!this._sanityProps.options){
+                return this.asset.url
+            }
+            let test=this.asset.url+"?"
+            Object.entries(this._sanityProps.options.imageOptions).forEach(function(e)  {try{test+=e.join("=")+"&"}catch{/*comment*/}})
+            return test
+        }
+},
+}
+</script>
