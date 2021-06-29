36c8836d874fe1e2a9c13c25808ef84db0d356ab
Bendik Knapstad <b.knapstad@gmaik.com>
2021-06-29 13:32:41 +0200

    Added scroll to top on routerlink

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -6,6 +6,9 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'history',
   routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView();
+}
 });
 
 export default router;
