6d65d8971bfffaf4fe4b35b61265c79ced25e59b
Bendik Knapstad <b.knapstad@gmaik.com>
2021-07-07 21:25:40 +0200

    add data ref

---
+++ site/src/components/Links.vue
@@ -0,0 +1,14 @@
+<template>
+    <pre>{{JSON.stringify(_sanityProps)}}</pre>
+</template>
+
+<script>
+export default {
+
+    name: "Links",
+    props: [ "_key", "_type", "href", "newtab", "_sanityProps" ],
+    computed: {
+        }
+}
+
+</script>
---
+++ site/src/components/LinksInternal.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{_sanityProps}}</p>
+</template>
+
+<script>
+export default {
+
+    name: "LinksInternal",
+    props: [ "_key", "_type", "_sanityProps" ],
+    computed: {
+        }
+}
+
+</script>
