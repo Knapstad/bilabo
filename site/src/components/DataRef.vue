7009e16126b942ad2e32cca970796b464a6f9ae5
Bendik Knapstad <b.knapstad@gmaik.com>
2021-07-07 21:25:40 +0200

    add data ref

---
+++ site/src/components/DataRef.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{_sanityProps}}</p>
+</template>
+
+<script>
+export default {
+
+    name: "DataRef",
+    props: [ "_key", "_type", "data", "_sanityProps" ],
+    computed: {
+        }
+}
+
+</script>
