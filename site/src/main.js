9e734c559b824702af0e5e84187c43d3b03f3dc0
Bendik Knapstad <b.knapstad@gmaik.com>
2021-09-21 10:11:18 +0200

    Add tracking to maib

--- site/src/main.js
+++ site/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VueMeta from 'vue-meta';
+import Tracking from './services/Tracking';
 
 Vue.config.productionTip = false;
 Vue.use(VueMeta,{
   refreshOnceOnNavigation: true
 });
+Vue.use(Tracking);
 new Vue({
   router,
   store,
