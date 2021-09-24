6ec6bc6f9f986dda3bc6f69c58f644e54806dbb5
Bendik Knapstad <b.knapstad@gmaik.com>
2021-09-24 09:30:46 +0200

    add selenium version of kinto.py

--- cloudFuctions/kinto.py
+++ cloudFuctions/kinto.py
@@ -1,7 +1,6 @@
-import requests
-import re
+from selenium import webdriver
 import json
-
+from bs4 import BeautifulSoup as BS
 
 class Kinto:
     def __init__(self):
@@ -11,50 +10,48 @@ class Kinto:
     def get_cars(cls, postalcode=None):
         try:
             base = "https://www.kinto.no"
-            api = "https://www.kinto.no/cars/all"
-            response = requests.get(f"{api}")
-            tries = 0
-            while "20" not in str(response.status_code):
-                response = requests.get(f"{api}", timeout=(2, 60))
-                tries += 1
-                if tries > 3:
-                    print("no response flex")
-                    return None
-            html = response.text
-            pattern = r"({\"dataManager\".*?)<\/script"
-            data = json.loads(re.search(pattern, html).group(1))
-            cars = data["query"]["cars"]
-
+            api = "https://www.kinto.no/produkter"
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu")
+            browser = webdriver.Chrome(options=chrome_options)
+            browser.get(api)
+            browser.find_element_by_css_selector(".LinkButton-sc-140e6lz-0").click()
+            links = [link.get_attribute("href") for link in browser.find_elements_by_class_name("ivbups") if "produkter/" in link.get_attribute("href")]
             cleanCars = []
             with open("car.json") as file:
                 template = json.load(file)
-            for car in cars:
-                if "hydrogen" in car.get("model",""):
-                            drive =  "hydrogen"
-                elif "electric" in car.get("model",""):
-                    drive = "elekrisk"
-                else:
-                    drive = "bensin"
+            for link in links:
+                browser.get(link)
+                extra_keys = browser.find_elements_by_css_selector(".gXQitm")
+                for i in extra_keys:
+                    browser.execute_script("arguments[0].click();", i)
+                extra_keys= [key.text for key in extra_keys]
+                extra_values = [value.text for value in browser.find_elements_by_css_selector("#accordion__panel-raa-0")]
+                extradict = dict(zip(extra_keys, extra_values))
                 cartemplate = template.copy()
                 cartemplate.update(
                     {
                         "site": "kinto",
-                        "name": f"{car.get('make','')} {car.get('model','')}",
-                        "make": car.get("make", ""),
-                        "model": car.get("model", ""),
-                        "drive": drive,
-                        "year": car.get("year", ""),
-                        "seats": car.get("seats", ""),
-                        "transmission": car.get("transmission", ""),
-                        "price": int(car.get("pricePerMonth", "")),
-                        "range": car.get("range", ""),
+                        "name": browser.find_element_by_css_selector("div.Box-sc-1ld0et1-0.Flex-sc-2sde10-0.kqoHdf.bILvfu > h2").text,
+                        "make": browser.find_element_by_css_selector("div.Box-sc-1ld0et1-0.Flex-sc-2sde10-0.kqoHdf.bILvfu > h2").text.split(" ")[0],
+                        "model": " ".join(browser.find_element_by_css_selector("div.Box-sc-1ld0et1-0.Flex-sc-2sde10-0.kqoHdf.bILvfu > h2").text.split(" ")[1:-1]),
+                        "drive": browser.find_element_by_css_selector(".src__CordSvg-sc-1fn61nl-16").find_element_by_xpath("./../..").text.split("\n")[-1].replace(" (Bensin)","").replace(" (Diesel)",""),
+                        "year": browser.find_element_by_css_selector(".src__CalendarSvg-sc-1fn61nl-13").find_element_by_xpath("../..").text.split("\n")[-1],
+                        "seats": browser.find_element_by_css_selector(".src__ManSvg-sc-1fn61nl-15").find_element_by_xpath("./../..").text.split("\n")[-1].split(" ")[0],
+                        "transmission":"automat",
+                        "price": int(browser.find_element_by_css_selector("#__next > main > div.Box-sc-1ld0et1-0.cQMKvc > div > section > div.Box-sc-1ld0et1-0.Flex-sc-2sde10-0.BarContainer-uhiasd-0.jZCGHd.bILvfu.guGAjd > div > section > div > div > div.Box-sc-1ld0et1-0.Flex-sc-2sde10-0.kqoHdf.bILvfu > div > p.Text-ng0wsq-0.cbJnND").text.replace(" ","").replace("kr/mnd","")),
+                        "range": "",
                         "kmMonth": "1500",
                         "location": ["Oslo"],
-                        "availability": car.get("available", False),
-                        "order": f"{base}/car/{car.get('id','')}",
-                        "img": f"{base}{car.get('image', '')}",
-                        "cargoVolume": car.get("cargoVolume", ""),
-                    }
+                        "availability": "avaiable",
+                        "order": link,
+                        "img": browser.find_element_by_css_selector("img").get_attribute("src"),
+                        "cargoVolume": "",
+                        "description": browser.find_element_by_css_selector("div.Box-sc-1ld0et1-0.ixXCJs > p").text,
+                        "details": extradict
+
+                        }
                 )
                 cleanCars.append(cartemplate)
 
@@ -62,5 +59,5 @@ class Kinto:
             unavailable = [car for car in cleanCars if not car.get("availability", 0)]
             return (available, unavailable)
         except Exception as e:
-            print(e)
+            print(e, link)
             return None
