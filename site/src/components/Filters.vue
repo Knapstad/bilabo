ddad42d9991e4149c655958de3499a3d99088974
Bendik Knapstad <b.knapstad@gmail.com>
2023-01-16 14:33:12 +0100

    Change filter

--- site/src/components/Filters.vue
+++ site/src/components/Filters.vue
@@ -1,43 +1,40 @@
 <template>
-<div class="filter-accordian">
-    <details open>
-    <summary>Filter</summary>
-    <div class="filter-wrap">
-        <div class="filter-group">
-          <div class="label">Sted</div>
-          <div class="filter-section">
-            <div v-for="item in locations()" :key="item">
-              <FilterItem :item="item" :type="'locations'" />
-            </div>
-          </div>
+  <div class="filter-accordian">
+    <p>Filter</p>
+    <details class="filter-group">
+      <summary class="label">Sted</summary>
+      <div class="filter-section">
+        <div v-for="item in locations()" :key="item">
+          <FilterItem :item="item" :type="'locations'" />
         </div>
-        <div class="filter-group">
-          <div class="label">Merke</div>
-          <div class="filter-section">
-            <div v-for="item in makes().sort()" :key="item">
-              <FilterItem :item="item" :type="'makes'" />
-            </div>
-          </div>
+      </div>
+    </details>
+    <details class="filter-group">
+      <summary class="label">Merke</summary>
+      <div class="filter-section">
+        <div v-for="item in makes().sort()" :key="item">
+          <FilterItem :item="item" :type="'makes'" />
         </div>
-        <div class="filter-group">
-          <div class="label">Type</div>
-          <div class="filter-section">
-            <div v-for="item in drive()" :key="item">
-              <FilterItem :item="item" :type="'drive'" />
-            </div>
-          </div>
+      </div>
+    </details>
+    <details class="filter-group">
+      <summary class="label">Type</summary>
+      <div class="filter-section">
+        <div v-for="item in drive()" :key="item">
+          <FilterItem :item="item" :type="'drive'" />
         </div>
-        <div class="filter-group">
-          <div class="label">Leverandør</div>
-          <div class="filter-section">
-            <div v-for="item in sites()" :key="item">
-              <FilterItem :item="item" :type="'sites'" />
-            </div>
-          </div>
+      </div>
+    </details>
+    <details class="filter-group">
+      <summary class="label">Leverandør</summary>
+      <div class="filter-section">
+        <div v-for="item in sites()" :key="item">
+          <FilterItem :item="item" :type="'sites'" />
         </div>
-    </div>
-</details>
-</div>
+      </div>
+    </details>
+
+  </div>
 </template>
 
 <script>
@@ -54,15 +51,15 @@ export default {
     return {
     };
   },
-  created(){
+  created() {
     let qp = new URLSearchParams(window.location.search);
-    for(let i of qp.keys()){
-      let filters=qp.get(i).split(",")
-      filters.forEach(filter =>  
-        this.$store.commit("addFilter",[i,filter.toLowerCase()]))
+    for (let i of qp.keys()) {
+      let filters = qp.get(i).split(",")
+      filters.forEach(filter =>
+        this.$store.commit("addFilter", [i, filter.toLowerCase()]))
     }
   },
-methods: {
+  methods: {
     locations: function () {
       let cars = Object.values(this.data["data"]).flat();
       cars = cars
@@ -92,7 +89,7 @@ methods: {
     drive: function () {
       let cars = Object.values(this.flat).flat();
       cars = cars
-        .map((a) => a.drive.toLowerCase().trim().replace("Electric","Elektrisk").replace("PHV","Hybrid")||"Hydrogen")
+        .map((a) => a.drive.toLowerCase().trim().replace("Electric", "Elektrisk").replace("PHV", "Hybrid") || "Hydrogen")
         .flat()
         .filter((v, i, a) => a.indexOf(v) === i);
       return cars;
@@ -102,17 +99,30 @@ methods: {
 </script>
 
 <style scoped>
+p {
+  margin-block-start: 0;
+  margin-block-end: 5px;
+  font-size: x-large;
+}
+
 summary {
   align-self: flex-start;
 }
+
+summary.label {
+  font-size: x-large;
+}
+
 .filter-wrap {
   display: flex;
   flex-wrap: wrap;
   margin-top: 1rem;
 }
+
 .filter-accordian {
   margin: auto;
   display: flex;
+  flex-direction: column;
   padding: 1rem;
   width: fit-content;
   min-width: 40%;
@@ -121,31 +131,37 @@ summary {
   margin-top: 1rem;
   box-shadow: 2px 2px 3px #000;
   border-radius: 2px;
-  
 }
-.filter-group{
+
+.filter-group {
   display: flex;
   flex-direction: column;
   margin-right: 3px;
-  box-shadow: 1px 2px 3px #000;
+
   padding-bottom: 5px;
   padding-right: 5px;
 }
+
 .filter-section {
   display: grid;
   grid-template-rows: repeat(3, 2.2rem);
   grid-auto-flow: column;
 }
+
 .label {
   background: var(--main-dark);
   padding-left: 5px;
   border-radius: 2px 2px 1px 1px;
   width: 100%;
-  overflow: hidden;}
+  overflow: hidden;
+}
+
 @media only screen and (max-width: 900px) {
   .filter-section {
     grid-template-rows: repeat(3, 2rem);
+
   }
+
   .filter-wrap {
     width: 100%;
     margin: 0;
@@ -159,37 +175,45 @@ summary {
   .filter-section {
     grid-template-rows: repeat(4, 2rem);
   }
+
   .filter-wrap {
-    width: 100%;
+    width: 90%;
     margin: 0;
     padding: 0;
     padding-bottom: .5rem;
     flex-wrap: wrap;
   }
+
   .filter-accordian {
-    width: 100%;
-    margin: 0;
-    padding: 0;
+    min-width: 90%;
+    /* margin: 0; */
+    /* padding: 0; */
     margin-block-end: 5px;
-    }
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
 }
 
 @media only screen and (max-width: 600px) {
   .filter-section {
     grid-template-rows: repeat(5, 2rem);
   }
+
   .filter-wrap {
-    width: 100%;
+    width: 90%;
     margin: 0;
     padding: 0;
     padding-bottom: .5rem;
     flex-wrap: wrap;
   }
 }
+
 @media only screen and (max-width: 500px) {
   .filter-section {
     grid-template-rows: repeat(7, 2rem);
   }
+
   .filter-wrap {
     width: 100%;
     margin: 0;
@@ -198,11 +222,13 @@ summary {
     flex-wrap: wrap;
   }
 }
+
 @media only screen and (max-width: 360px) {
   .filter-section {
     grid-template-rows: repeat(10, 1.5rem);
     /* grid-auto-flow: columns; */
   }
+
   .filter-wrap {
     width: 100%;
     margin: 0;
