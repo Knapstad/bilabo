a44bd1284acfa4ae4f27315d210c5e7be6356205
Bendik Knapstad <b.knapstad@gmail.com>
2023-02-23 12:45:43 +0100

    change car url

--- site/src/components/Car.vue
+++ site/src/components/Car.vue
@@ -1,43 +1,30 @@
 <template>
   <div class="wrap">
-    <div class="header" :id="id"><h3>{{ car.name }}</h3>
-      <img class="logo" :src="logo" :alt='"logo for "+ car.site'>
+    <div class="header" :id="id">
+      <h3>{{ car.name }}</h3>
+      <img class="logo" :src="logo" :alt='"logo for " + car.site'>
     </div>
     <div class="hello">
-      <div class="column">
+      <div class="column info">
         <p>Pris: {{ car.price }}/mnd (inkl. mva)</p>
         <p>{{ car.drive }}</p>
-        <p v-if="car.includedkm">KM/mnd: {{parseInt(car.includedkm)}}</p>
-        <p v-if="car.kmMonth">KM/mnd: {{car.kmMonth}}</p>
+        <p v-if="car.includedkm">KM/mnd: {{ parseInt(car.includedkm) }}</p>
+        <p v-if="car.kmMonth">KM/mnd: {{ car.kmMonth }}</p>
         <p v-if="car.delivery">{{ car.delivery }} levering</p>
       </div>
-      <div class="column">
-          <svg
-          v-if="car.img === 'svg'"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          version="1.1"
-          id="Capa_1"
-          x="0px"
-          y="0px"
-          width="100%"
-          height="100%"
-          viewBox="0 10 100 65"
-          style="enable-background: new 0 0 98.967 98.967"
-          xml:space="preserve"
-          alt="Illustrasjon av bil ved manglende bilde"
-        >
+      <div class="column image">
+        <svg class="carimage" v-if="car.img === 'svg'" xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="100%" height="100%"
+          viewBox="0 10 100 65" style="enable-background: new 0 0 98.967 98.967" xml:space="preserve"
+          alt="Illustrasjon av bil ved manglende bilde">
           <g>
             <g>
               <path
-                d="M17.275,52.156c-4.124,0-7.468,3.343-7.468,7.468c0,0.318,0.026,0.631,0.066,0.938c0.463,3.681,3.596,6.528,7.401,6.528    c3.908,0,7.112-3.004,7.437-6.83c0.017-0.209,0.031-0.422,0.031-0.637C24.743,55.499,21.4,52.156,17.275,52.156z M13.537,56.81    l1.522,1.523c-0.118,0.203-0.211,0.422-0.271,0.656h-2.146C12.752,58.177,13.063,57.435,13.537,56.81z M12.632,60.282h2.163    c0.061,0.23,0.151,0.448,0.271,0.648l-1.526,1.525C13.067,61.835,12.749,61.093,12.632,60.282z M16.629,64.263    c-0.809-0.113-1.544-0.43-2.166-0.899l1.518-1.519c0.2,0.117,0.419,0.203,0.648,0.263V64.263z M16.629,57.14    c-0.235,0.062-0.455,0.154-0.66,0.275l-1.521-1.521c0.625-0.475,1.367-0.789,2.181-0.902V57.14z M17.922,54.99    c0.814,0.113,1.557,0.429,2.181,0.903l-1.52,1.521c-0.204-0.121-0.426-0.213-0.66-0.275L17.922,54.99L17.922,54.99z     M17.922,64.261v-2.152c0.23-0.061,0.447-0.146,0.647-0.264l1.519,1.519C19.466,63.833,18.73,64.148,17.922,64.261z     M21.014,62.462l-1.531-1.533c0.12-0.201,0.217-0.416,0.278-0.646h2.146C21.793,61.091,21.488,61.839,21.014,62.462z     M19.764,58.989c-0.061-0.234-0.153-0.453-0.271-0.656l1.524-1.523c0.471,0.625,0.782,1.367,0.894,2.18H19.764z"
-              />
+                d="M17.275,52.156c-4.124,0-7.468,3.343-7.468,7.468c0,0.318,0.026,0.631,0.066,0.938c0.463,3.681,3.596,6.528,7.401,6.528    c3.908,0,7.112-3.004,7.437-6.83c0.017-0.209,0.031-0.422,0.031-0.637C24.743,55.499,21.4,52.156,17.275,52.156z M13.537,56.81    l1.522,1.523c-0.118,0.203-0.211,0.422-0.271,0.656h-2.146C12.752,58.177,13.063,57.435,13.537,56.81z M12.632,60.282h2.163    c0.061,0.23,0.151,0.448,0.271,0.648l-1.526,1.525C13.067,61.835,12.749,61.093,12.632,60.282z M16.629,64.263    c-0.809-0.113-1.544-0.43-2.166-0.899l1.518-1.519c0.2,0.117,0.419,0.203,0.648,0.263V64.263z M16.629,57.14    c-0.235,0.062-0.455,0.154-0.66,0.275l-1.521-1.521c0.625-0.475,1.367-0.789,2.181-0.902V57.14z M17.922,54.99    c0.814,0.113,1.557,0.429,2.181,0.903l-1.52,1.521c-0.204-0.121-0.426-0.213-0.66-0.275L17.922,54.99L17.922,54.99z     M17.922,64.261v-2.152c0.23-0.061,0.447-0.146,0.647-0.264l1.519,1.519C19.466,63.833,18.73,64.148,17.922,64.261z     M21.014,62.462l-1.531-1.533c0.12-0.201,0.217-0.416,0.278-0.646h2.146C21.793,61.091,21.488,61.839,21.014,62.462z     M19.764,58.989c-0.061-0.234-0.153-0.453-0.271-0.656l1.524-1.523c0.471,0.625,0.782,1.367,0.894,2.18H19.764z" />
               <path
-                d="M79.284,52.156c-4.124,0-7.468,3.343-7.468,7.468c0,0.318,0.026,0.631,0.066,0.938c0.463,3.681,3.596,6.528,7.4,6.528    c3.908,0,7.112-3.004,7.438-6.83c0.017-0.209,0.031-0.422,0.031-0.637C86.753,55.499,83.409,52.156,79.284,52.156z M75.546,56.81    l1.521,1.523c-0.118,0.203-0.211,0.422-0.271,0.656H74.65C74.761,58.177,75.072,57.435,75.546,56.81z M74.642,60.282h2.163    c0.061,0.23,0.151,0.448,0.271,0.648l-1.525,1.525C75.076,61.835,74.757,61.093,74.642,60.282z M78.638,64.263    c-0.809-0.113-1.544-0.43-2.166-0.899l1.518-1.519c0.2,0.117,0.419,0.203,0.648,0.263V64.263z M78.638,57.14    c-0.235,0.062-0.455,0.154-0.66,0.275l-1.521-1.521c0.625-0.475,1.366-0.789,2.181-0.902V57.14z M79.932,54.99    c0.814,0.113,1.557,0.429,2.181,0.903l-1.521,1.521c-0.204-0.121-0.426-0.215-0.66-0.275V54.99z M79.932,64.261v-2.152    c0.23-0.061,0.447-0.146,0.647-0.264l1.519,1.519C81.476,63.833,80.739,64.148,79.932,64.261z M83.023,62.462l-1.531-1.531    c0.12-0.202,0.218-0.416,0.278-0.647h2.146C83.802,61.091,83.498,61.839,83.023,62.462z M81.773,58.989    c-0.061-0.234-0.152-0.453-0.271-0.656l1.523-1.523c0.472,0.625,0.782,1.367,0.895,2.18H81.773z"
-              />
+                d="M79.284,52.156c-4.124,0-7.468,3.343-7.468,7.468c0,0.318,0.026,0.631,0.066,0.938c0.463,3.681,3.596,6.528,7.4,6.528    c3.908,0,7.112-3.004,7.438-6.83c0.017-0.209,0.031-0.422,0.031-0.637C86.753,55.499,83.409,52.156,79.284,52.156z M75.546,56.81    l1.521,1.523c-0.118,0.203-0.211,0.422-0.271,0.656H74.65C74.761,58.177,75.072,57.435,75.546,56.81z M74.642,60.282h2.163    c0.061,0.23,0.151,0.448,0.271,0.648l-1.525,1.525C75.076,61.835,74.757,61.093,74.642,60.282z M78.638,64.263    c-0.809-0.113-1.544-0.43-2.166-0.899l1.518-1.519c0.2,0.117,0.419,0.203,0.648,0.263V64.263z M78.638,57.14    c-0.235,0.062-0.455,0.154-0.66,0.275l-1.521-1.521c0.625-0.475,1.366-0.789,2.181-0.902V57.14z M79.932,54.99    c0.814,0.113,1.557,0.429,2.181,0.903l-1.521,1.521c-0.204-0.121-0.426-0.215-0.66-0.275V54.99z M79.932,64.261v-2.152    c0.23-0.061,0.447-0.146,0.647-0.264l1.519,1.519C81.476,63.833,80.739,64.148,79.932,64.261z M83.023,62.462l-1.531-1.531    c0.12-0.202,0.218-0.416,0.278-0.647h2.146C83.802,61.091,83.498,61.839,83.023,62.462z M81.773,58.989    c-0.061-0.234-0.152-0.453-0.271-0.656l1.523-1.523c0.472,0.625,0.782,1.367,0.895,2.18H81.773z" />
               <path
-                d="M97.216,48.29v-5.526c0-0.889-0.646-1.642-1.524-1.779c-2.107-0.33-5.842-0.953-7.52-1.47    c-2.406-0.742-11.702-4.678-14.921-5.417c-3.22-0.739-17.738-4.685-31.643,0.135c-2.353,0.815-12.938,5.875-19.162,8.506    c-1.833,0.04-19.976,3.822-20.942,6.414c-0.966,2.593-1.269,3.851-1.447,4.509c-0.178,0.658,0,3.807,1.348,6    c1.374,0.777,4.019,1.299,7.077,1.649c-0.035-0.187-0.073-0.371-0.097-0.56c-0.053-0.404-0.078-0.773-0.078-1.125    c0-4.945,4.022-8.969,8.968-8.969s8.968,4.023,8.968,8.969c0,0.254-0.017,0.506-0.036,0.754c-0.047,0.555-0.147,1.094-0.292,1.613    c0.007,0,0.024,0,0.024,0l44.516-0.896c-0.02-0.115-0.046-0.229-0.061-0.346c-0.053-0.402-0.078-0.772-0.078-1.125    c0-4.945,4.022-8.968,8.968-8.968c4.946,0,8.969,4.022,8.969,8.968c0,0.019-0.002,0.035-0.003,0.053l0.19-0.016l7.611-1.433    c0,0,2.915-1.552,2.915-5.822C98.967,49.56,97.216,48.29,97.216,48.29z M53.057,43.051L36.432,43.56    c0.306-2.491-1.169-3.05-1.169-3.05c6.609-5.999,19.929-6.202,19.929-6.202L53.057,43.051z M71.715,42.29l-15.15,0.509l1.373-8.49    c7.83-0.102,12.303,1.626,12.303,1.626l2.237,3.61L71.715,42.29z M80.256,42.238h-4.221l-4.22-5.795    c3.166,1.26,5.7,2.502,7.209,3.287C79.94,40.206,80.44,41.223,80.256,42.238z"
-              />
+                d="M97.216,48.29v-5.526c0-0.889-0.646-1.642-1.524-1.779c-2.107-0.33-5.842-0.953-7.52-1.47    c-2.406-0.742-11.702-4.678-14.921-5.417c-3.22-0.739-17.738-4.685-31.643,0.135c-2.353,0.815-12.938,5.875-19.162,8.506    c-1.833,0.04-19.976,3.822-20.942,6.414c-0.966,2.593-1.269,3.851-1.447,4.509c-0.178,0.658,0,3.807,1.348,6    c1.374,0.777,4.019,1.299,7.077,1.649c-0.035-0.187-0.073-0.371-0.097-0.56c-0.053-0.404-0.078-0.773-0.078-1.125    c0-4.945,4.022-8.969,8.968-8.969s8.968,4.023,8.968,8.969c0,0.254-0.017,0.506-0.036,0.754c-0.047,0.555-0.147,1.094-0.292,1.613    c0.007,0,0.024,0,0.024,0l44.516-0.896c-0.02-0.115-0.046-0.229-0.061-0.346c-0.053-0.402-0.078-0.772-0.078-1.125    c0-4.945,4.022-8.968,8.968-8.968c4.946,0,8.969,4.022,8.969,8.968c0,0.019-0.002,0.035-0.003,0.053l0.19-0.016l7.611-1.433    c0,0,2.915-1.552,2.915-5.822C98.967,49.56,97.216,48.29,97.216,48.29z M53.057,43.051L36.432,43.56    c0.306-2.491-1.169-3.05-1.169-3.05c6.609-5.999,19.929-6.202,19.929-6.202L53.057,43.051z M71.715,42.29l-15.15,0.509l1.373-8.49    c7.83-0.102,12.303,1.626,12.303,1.626l2.237,3.61L71.715,42.29z M80.256,42.238h-4.221l-4.22-5.795    c3.166,1.26,5.7,2.502,7.209,3.287C79.94,40.206,80.44,41.223,80.256,42.238z" />
             </g>
           </g>
           <g />
@@ -56,62 +43,66 @@
           <g />
           <g />
         </svg>
-        <img v-else :src="car.img.replace('/upload/','/upload/f_auto,q_auto:eco,w_500/')"  :alt="car.name + ' ' + car.year + '-modell'" @error="imageLoadError"/>
+        <img v-else class="carimage" :src="car.img.replace('/upload/', '/upload/f_auto,q_auto:eco,w_500/e_trim/')"
+          :alt="car.name + ' ' + car.year + '-modell'" @error="imageLoadError" />
       </div>
     </div>
     <p v-if="car.site === 'volvo'" class="carfooter">
-      Motor: {{car.enginDescription}}
+      Motor: {{ car.enginDescription }}
       Forbruk: {{ car.fuelconsumption }}
       Utslipp: {{ car.co2 }}
       Binding: {{ car.binding }}
     </p>
-  <div class=buttoncontainer>
-    <a class="button left" :href="url" target="_blank" rel="nofollow noopener" :aria-label= "'Bestill '+ car.name" >
-      BESTILL 
+    <div class=buttoncontainer>
+      <a class="button left" :href="url" target="_blank" rel="nofollow noopener" :aria-label="'Bestill ' + car.name">
+        BESTILL
       </a>
-    <router-link class="button" :to="{name: 'cardetails', params:{site: car.site, carname: car.name.replaceAll(' ','-')+'-id-'+car.id}}"  :aria-label= "'Les mer om '+ car.name" >
-    LES MER 
+      <router-link class="button"
+        :to="{ name: 'cardetails', params: { site: car.site, carname: car.name.replaceAll(' ', '-') + '-id-' + car.id } }"
+        :aria-label="'Les mer om ' + car.name">
+        LES MER
       </router-link>
-  </div>
+    </div>
   </div>
 </template> 
 
 <script>
 export default {
-  name: 'Car',  
-  props: {  
+  name: 'Car',
+  props: {
     car: Object,
     site: String,
     id: Number,
   },
   methods: {
-    imageLoadError () {
+    imageLoadError() {
       this.car.img = "svg";
-      }
-    },
+    }
+  },
   computed: {
     url() {
-      if (this.car.order.includes('?')) {
+      if (this.car.site == "volvo") {
+        return (
+          this.car.order
+        );
+      } else if (this.car.order.includes('?')) {
         return (
           this.car.order +
           '&utm_source=bilabonnemet.app&utm_medium=link&utm_campaign=bilabonnement.app'
         );
-      } else if(this.car.site == "volvo"){
-        return (
-          this.car.order
-        );
+
       } else {
         return this.car.order +
           '?utm_source=bilabonnemet.app&utm_medium=link&utm_campaign=bilabonnement.app'
-        
+
       }
     },
-    logo(){
+    logo() {
       let logo = ""
-      try{
+      try {
         logo = require(`@/assets/${this.car.site}.png`);
-      }catch{
-        logo="";
+      } catch {
+        logo = "";
       }
       return logo
     }
@@ -126,27 +117,33 @@ export default {
   height: 100%;
   overflow: hidden;
 }
+
 .column {
   flex: 50%;
   max-width: 50%;
   margin: auto;
 }
-.header{
+
+.header {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 }
+
 h3 {
   padding-left: 5%;
   padding-bottom: 0;
   margin-bottom: 0;
 }
+
 p {
   padding-left: 10%;
 }
+
 a {
   text-decoration: none;
 }
+
 .button {
   text-decoration: none;
   text-align: center;
@@ -158,29 +155,37 @@ a {
   background: var(--main-medium-dark);
   margin-top: 5px;
 }
-.button.left{
+
+.button.left {
   border-right: var(--main-dark) 1px solid;
 }
+
 .button:hover {
-  background: var(--main-dark); 
+  background: var(--main-dark);
 }
+
 .button:focus {
   background: var(--main-dark);
 }
+
 .buttoncontainer {
   display: flex;
+  align-items: flex-end;
 }
+
 img {
   width: 100%;
   height: auto;
   padding-top: 3px;
-  
+
 }
-img.logo{
+
+img.logo {
   width: 56px;
   margin-right: 10px;
   align-self: center;
 }
+
 svg {
   width: 98%;
   padding-top: 3px;
@@ -188,6 +193,7 @@ svg {
 }
 
 .wrap {
+
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -199,18 +205,69 @@ svg {
   padding-top: 0px;
   /* margin-bottom: 4px; */
 }
+
 .label {
   padding-left: 10%;
   display: flex;
 }
+
 .location-container {
   display: flex;
   flex-wrap: wrap;
 }
-.carfooter{
-font-size: 0.75rem;
+
+.carfooter {
+  font-size: 0.75rem;
 }
-p.carfooter{
+
+p.carfooter {
   padding-left: 5%;
 }
-</style>
+
+@media only screen and (max-width: 740px) {
+  .wrap {
+    /* background-color: red; */
+    display: grid;
+    grid-template-columns: 50% 50%;
+
+
+  }
+
+  .header {
+    grid-column: 1/3;
+    grid-row: 1;
+    z-index: 1;
+    background-color: rgba(var(--main-light));
+
+  }
+
+  .header h3 {
+    padding-top: 0;
+    margin-top: 5px;
+
+  }
+
+  .buttoncontainer {
+    grid-column: 1 / 3;
+    grid-row: 3 4;
+    width: 100%;
+  }
+
+  .hello {
+    grid-column: 1/3;
+    grid-row: 1/4;
+    display: flex;
+    flex-direction: column-reverse;
+    width: 100%;
+
+  }
+
+  .column {
+    max-width: 100%;
+    margin: 0;
+  }
+
+  .carimage {
+    padding: 0;
+  }
+}</style>
